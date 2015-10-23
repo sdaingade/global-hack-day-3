d51b74b98ea8da51342f00160a4b14832126c6e4
Swapnil Daingade <sdaingade@maprtech.com>
2015-10-23 05:17:06 -0700

    Addiding additional params to docker deploy script
    Sample usage
    
    ./docker_deploy.sh -TYPE RM -NAME rm2 -CLUSTER red -IMAGE maprdocker.lab/hackday-ubuntu:rm -ZK_ADDR_EXT 10.10.30.52:5181 -HOST_IP 10.10.30.52 -SCHEDULER_PORT 9001 -RMUI_PORT 9002 -MYRIADUI_PORT 9003

--- OnDemandYARNClusters/scripts/docker_deploy.sh
+++ OnDemandYARNClusters/scripts/docker_deploy.sh
@@ -6,16 +6,37 @@
 
 echo "Starting deploying script"
 
-#ARG1: taskName given by Mesos
-taskName=$1
-#ARG2: YARN cluster to which this node belongs, E.g. Red cluster, Blue cluster etc
-clusterId=$2
-#ARG3: Image to be deployed by docker.
-image=$3
-#ARG4: Zookeeper address (External)
-zkAddr=$4
+while [ $# -gt 0 ]
+do
+  case "$1" in
+  -TYPE) shift
+         containerType=$1;;
+  -NAME) shift
+         taskName=$1;;  #taskName given by Mesos
+  -CLUSTER) shift
+         clusterId=$1;; #YARN cluster to which this node belongs, E.g. Red cluster, Blue cluster etc
+  -IMAGE) shift
+         image=$1;;     #Image to be deployed by docker.
+  -ZK_ADDR_EXT) shift
+         zkAddrExt=$1;; #Zookeeper address (External)
+  -HOST_IP) shift
+         hostIp=$1;;
+  -SCHEDULER_PORT) shift
+         schedulerPort=$1;;
+  -RMUI_PORT) shift
+         rmuiPort=$1;;
+  -MYRIADUI_PORT) shift
+         myriaduiPort=$1;;
+  -NM_OCTET) shift
+         nmOctet=$1;;
+  esac
+  shift
+done
 
-docker_env=(-e "ZK_ADDR_EXT=$zkAddr")
+
+# Pre container launch actions
+
+docker_env=(-e "ZK_ADDR_EXT=$zkAddrExt" -e "HOST_IP=$hostIp" -e "SCHEDULER_PORT=$schedulerPort")
 
 # TODO Make loop back device to be exposed to container configurable
 device="/dev/loop0"
@@ -47,6 +68,12 @@ cid=${cid:4:5}
 bridgeIP=`ip addr show $clusterId | grep 'inet .* global' | awk '{print $2}' | sed 's/\/16//'`
 RM_IP=`ip addr show $clusterId | grep 'inet .* global' | awk '{print $2}' | sed 's/1\/16/2/'`
 
+if [ "$containerType" = "RM" ]; then
+  containerIp=`ip addr show $clusterId | grep 'inet .* global' | awk '{print $2}' | sed 's/1\/16/2/'`
+else
+  containerIp==`ip addr show $clusterId | grep 'inet .* global' | awk '{print $2}' | sed 's/1\/16/${nmOctet}/'` 
+fi
+
 # Create a veth pair 
 ip link add $clusterId-$cid-0 type veth peer name $clusterId-$cid-1
 
@@ -59,10 +86,29 @@ ip link set $clusterId-$cid-1 netns $pid
 # Rename to eth1
 ip netns exec $pid ip link set dev $clusterId-$cid-1 name eth1
 ip netns exec $pid ip link set eth1 up
-ip netns exec $pid ip addr add $RM_IP/16 dev eth1
+ip netns exec $pid ip addr add $containerIp/16 dev eth1
 # Set default route via bridge (eth1). Delete existing (eth0)
 ip netns exec $pid ip route delete default
 ip netns exec $pid ip route add default via $bridgeIP
+
+
+# Post container launch actions
+if [ "$containerType" = "RM" ]; then
+  iptables -A DOCKER -d ${RM_IP}/32 ! -i $clusterId -o $clusterId -p tcp -m tcp --dport $schedulerPort -j ACCEPT
+  iptables -t nat -A POSTROUTING -s ${RM_IP}/32 -d ${RM_IP}/32 -p tcp -m tcp --dport $schedulerPort -j MASQUERADE
+  iptables -t nat -A DOCKER -p tcp -m tcp --dport $schedulerPort -j DNAT --to-destination ${RM_IP}:${schedulerPort}
+
+  #RM UI port
+  iptables -A DOCKER -d ${RM_IP}/32 ! -i $clusterId -o $clusterId -p tcp -m tcp --dport 8088 -j ACCEPT
+  iptables -t nat -A POSTROUTING -s ${RM_IP}/32 -d ${RM_IP}/32 -p tcp -m tcp --dport 8088 -j MASQUERADE
+  iptables -t nat -A DOCKER -p tcp -m tcp --dport $rmuiPort -j DNAT --to-destination ${RM_IP}:8088
+
+  #Myriad UI
+  iptables -A DOCKER -d ${RM_IP}/32 ! -i $clusterId -o $clusterId -p tcp -m tcp --dport 8192 -j ACCEPT
+  iptables -t nat -A POSTROUTING -s ${RM_IP}/32 -d ${RM_IP}/32 -p tcp -m tcp --dport 8192 -j MASQUERADE
+  iptables -t nat -A DOCKER -p tcp -m tcp --dport $myriaduiPort -j DNAT --to-destination ${RM_IP}:8192
+fi
+
 
 #TODO Check if Mesos task exits after this script has finished running.
 # If yes do something like this to keep script from terminating
